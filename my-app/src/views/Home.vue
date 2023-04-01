62c8a7f5fde2d8935b8078f5979d088bd50bc32f
叶振权 <879545485@qq.com>
2023-04-01 13:36:31 +0800

    首页echarts图实现

--- my-app/src/views/Home.vue
+++ my-app/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home">
     <el-row>
-      <el-col :span="8">
+      <el-col :span="8" style="padding-right: 10px">
         <el-card class="box-card">
           <div class="user">
             <img
@@ -21,12 +21,45 @@
         </el-card>
         <el-card class="box-card buyInfo">
           <el-table :data="tableData" style="width: 100%">
-            <el-table-column v-for="(val,key) in tableLabel" :prop="key" :label="val" />
+            <el-table-column
+              v-for="(val, key) in tableLabel"
+              :key="key"
+              :prop="key"
+              :label="val"
+            />
           </el-table>
         </el-card>
       </el-col>
-      <el-col :span="16"> 
-
+      <el-col :span="16" style="padding-left: 10px">
+        <div class="num">
+          <el-card
+            class="box-card"
+            :body-style="{ display: 'flex', padding: 0 }"
+            v-for="item in countData"
+            :key="item.name"
+          >
+            <i
+              class="icon"
+              :class="`el-icon-${item.icon}`"
+              :style="{ background: item.color }"
+            ></i>
+            <div class="detail">
+              <p class="value">￥{{ item.value }}</p>
+              <p class="name">{{ item.name }}</p>
+            </div>
+          </el-card>
+        </div>
+        <el-card class="box-card" style="height: 280px">
+          <div ref="echarts_a" style="height: 280px"></div>
+        </el-card>
+        <div class="graph">
+          <el-card class="box-card" style="height: 260px; width: 48%">
+            <div ref="echarts_b" style="height: 260px"></div>
+          </el-card>
+          <el-card class="box-card" style="height: 260px; width: 48%">
+            <div ref="echarts_c" style="height: 240px"></div>
+          </el-card>
+        </div>
       </el-col>
     </el-row>
   </div>
@@ -65,70 +98,171 @@
       }
     }
   }
-  .buyInfo{
+  .buyInfo {
     margin-top: 20px;
     height: 460px;
+  }
+  .num {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    .box-card {
+      width: 32%;
+      margin-bottom: 20px;
+    }
+    .icon {
+      font-size: 30px;
+      width: 80px;
+      height: 80px;
+      text-align: center;
+      line-height: 80px;
+      color: #fff;
+    }
+    .detail {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      margin-left: 15px;
+      .value {
+        font-size: 30px;
+        margin-bottom: 10px;
+        line-height: 30px;
+      }
+      .name {
+        font-size: 14px;
+        text-align: center;
+        color: #999;
+      }
+    }
+  }
+  .graph {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
   }
 }
 </style>
 
 <script>
+import { getData } from "../api";
+import * as echarts from "echarts";
 export default {
   data() {
     return {
-        tableData: [
-            {
-                name:'oppo',
-                todayBuy:100,
-                monthBuy:300,
-                totalBuy:800,
-            },
-            {
-                name:'vivo',
-                todayBuy:100,
-                monthBuy:300,
-                totalBuy:800,
-            },
-            {
-                name:'苹果',
-                todayBuy:100,
-                monthBuy:300,
-                totalBuy:800,
-            },
-            {
-                name:'小米',
-                todayBuy:100,
-                monthBuy:300,
-                totalBuy:800,
-            },
-            {
-                name:'三星',
-                todayBuy:100,
-                monthBuy:300,
-                totalBuy:800,
-            },
-            {
-                name:'魅族',
-                todayBuy:100,
-                monthBuy:300,
-                totalBuy:800,
-            }
-        ],
-        tableLabel:{
-            name:'课程',
-            todayBuy:'今日购买',
-            monthBuy:'本月购买',
-            totalBuy:'总购买',
-        },
-        countData:[
-            {
-                name:'今日支付订单',
-                value:1234,
-                icon:'succsee',
-                color:'#2ec7c9',
-            },
-        ]
+      tableData: [],
+      tableLabel: {
+        name: "课程",
+        todayBuy: "今日购买",
+        monthBuy: "本月购买",
+        totalBuy: "总购买",
+      },
+      countData: [],
     };
+  },
+  mounted() {
+    getData().then(({ data }) => {
+      const { tableData, countData } = data.data;
+      this.tableData = tableData;
+      this.countData = countData;
+      //折线图
+      const echarts_a = echarts.init(this.$refs.echarts_a);
+      var echarts_a_option = {};
+      const { orderData, userData, videoData } = data.data;
+      const xAxis = Object.keys(orderData.data[0]);
+      echarts_a_option.xAxis = {
+        data: xAxis,
+      };
+      echarts_a_option.legend = {
+        data: xAxis,
+      };
+      echarts_a_option.yAxis = {};
+      echarts_a_option.series = [];
+      xAxis.forEach((key) => {
+        echarts_a_option.series.push({
+          name: key,
+          data: orderData.data.map((item) => item[key]),
+          type: "line",
+        });
+      });
+      echarts_a.setOption(echarts_a_option);
+      //柱状图
+      const echarts_b = echarts.init(this.$refs.echarts_b);
+      var echarts_b_option = {
+        legend: {
+          // 图例文字颜色
+          textStyle: {
+            color: "#333",
+          },
+        },
+        grid: {
+          left: "20%",
+        },
+        // 提示框
+        tooltip: {
+          trigger: "axis",
+        },
+        xAxis: {
+          type: "category", // 类目轴
+          data: userData.map((item) => item.date),
+          axisLine: {
+            lineStyle: {
+              color: "#17b3a3",
+            },
+          },
+          axisLabel: {
+            interval: 0,
+            color: "#333",
+          },
+        },
+        yAxis: [
+          {
+            type: "value",
+            axisLine: {
+              lineStyle: {
+                color: "#17b3a3",
+              },
+            },
+          },
+        ],
+        color: ["#2ec7c9", "#b6a2de"],
+        series: [
+          {
+            name: "新增用户",
+            data: userData.map((item) => item.new),
+            type: "bar",
+          },
+          {
+            name: "活跃用户",
+            data: userData.map((item) => item.active),
+            type: "bar",
+          },
+        ],
+      };
+      echarts_b.setOption(echarts_b_option);
+      //饼状图
+      const echarts_c = echarts.init(this.$refs.echarts_c);
+      var echarts_c_option = {
+        tooltip: {
+          trigger: "item",
+        },
+        color: [
+          "#0f78f4",
+          "#dd536b",
+          "#9462e5",
+          "#a6a6a6",
+          "#e1bb22",
+          "#39c362",
+          "#3ed1cf",
+        ],
+        series: [
+          {
+            data: videoData,
+            type: "pie",
+          },
+        ],
+      };
+      echarts_c.setOption(echarts_c_option);
+    });
   },
 };
 </script>
